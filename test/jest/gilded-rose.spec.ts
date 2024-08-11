69a7ad8ac6a89d9ce3b9a43ce394cf75ab341b55
Süleyman  Taşpınar <suleythegreat@Suleymans-MacBook-Pro.local>
2024-08-11 16:05:27 +0300

    test: add tests for backstage

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -71,3 +71,37 @@ describe('Sulfuras, Hand of Ragnaros Rules', () => {
     expect(items[0].sellIn).toBe(2);
   });
 });
+
+describe('Backstage passes Rules', () => {
+  it('should increase in quality as its sellIn value approaches', () => {
+    const gildedRose = new GildedRose([
+      new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(21);
+  });
+
+  it('should increase in quality by 2 when there are 10 days or less', () => {
+    const gildedRose = new GildedRose([
+      new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(22);
+  });
+
+  it('should increase in quality by 3 when there are 5 days or less', () => {
+    const gildedRose = new GildedRose([
+      new Item('Backstage passes to a TAFKAL80ETC concert', 5, 20),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(23);
+  });
+
+  it('should drop the quality to 0 after the concert', () => {
+    const gildedRose = new GildedRose([
+      new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20),
+    ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+});
