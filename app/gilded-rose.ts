c74460d665771898d48f68c490fc2337d3f96e66
Süleyman  Taşpınar <suleythegreat@Suleymans-MacBook-Pro.local>
2024-08-11 17:54:25 +0300

    refactor: encapsulate aged brie logic

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,6 +1,6 @@
 import {Item} from './item'
 
-import { updateQualityForConcertItem } from './update-strategies';
+import { updateQualityForConcertItem, updateQualityForAgedBrie } from './update-strategies';
 
 export class GildedRose {
   items: Array<Item>;
@@ -15,16 +15,15 @@ export class GildedRose {
         case 'Backstage passes to a TAFKAL80ETC concert':
           item = updateQualityForConcertItem(item)
           break;
+        case 'Aged Brie':
+            item = updateQualityForAgedBrie(item)
+            break;
         default:
           if (!['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros'].includes(item.name)
             && item.quality > 0 ) { // decrase quailty
             item.quality = item.quality - 1
           }
 
-          if(['Aged Brie'].includes(item.name) && item.quality < 50) { // increase quality
-            item.quality = item.quality + 1
-          }
-    
           if (item.name != 'Sulfuras, Hand of Ragnaros' && item.name !='Backstage passes to a TAFKAL80ETC concert' ) { // decrease sellIn
             item.sellIn = item.sellIn - 1;
           }
@@ -33,13 +32,7 @@ export class GildedRose {
             && item.sellIn < 0  && item.quality > 0) {
             item.quality = item.quality - 1
           }
-    
-          if(item.sellIn < 0 && item.name =='Aged Brie' && item.quality < 50) {
-            console.log("5")
-            item.quality = item.quality + 1
-          }
       }
-
     });
 
     return this.items;
--- app/update-strategies.ts
+++ app/update-strategies.ts
@@ -20,3 +20,17 @@ export const updateQualityForConcertItem = (item: Item) :Item => {
 
   return item
 }
+
+export const updateQualityForAgedBrie = (item: Item) :Item => {
+  if(item.quality < 50) {
+    item.quality = item.quality + 1
+  }
+
+  item.sellIn = item.sellIn - 1;
+
+  if(item.sellIn < 0 && item.quality < 50) {
+    item.quality = item.quality + 1
+  }
+
+  return item
+}
