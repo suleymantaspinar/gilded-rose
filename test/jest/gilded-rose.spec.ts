a5779c1eb4275578b4e289d0776f78348c38cb76
Süleyman  Taşpınar <suleythegreat@Suleymans-MacBook-Pro.local>
2024-08-11 15:50:45 +0300

    test: add tests for common rules

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -24,25 +24,17 @@ describe('Gilded Rose', () => {
 });
 
 
-describe('Gilded Rose', () => {
-  it('should add a new item', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-
+describe('Common Rules', () => {
+  it('should not update the quality if it is 0', () => {
+    const gildedRose = new GildedRose([new Item('foo', 10, 0)]);
     const items = gildedRose.updateQuality();
-
-    expect(items[0].name).toBe('foo');
+    expect(items[0].quality).toBe(0);
   });
 
-  it('should add multiple items', () => {
-    const gildedRose = new GildedRose([
-      new Item('foo', 0, 0),
-      new Item('bar', 0, 0)
-    ]);
-
+  it('should degrades twice once sell by date has passed', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 10)]);
     const items = gildedRose.updateQuality();
-
-    expect(items[0].name).toBe('foo');
-    expect(items[1].name).toBe('bar');
+    expect(items[0].sellIn).toBe(-1);
+    expect(items[0].quality).toBe(8);
   });
-
 });
