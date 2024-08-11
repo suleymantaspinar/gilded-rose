38b958aad85d933bc0f0509544e11e8a610424ab
Süleyman  Taşpınar <suleythegreat@Suleymans-MacBook-Pro.local>
2024-08-11 17:24:23 +0300

    refactor: consalidate if logics

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -19,12 +19,12 @@ export class GildedRose {
 
   updateQuality() :Item[] {
     this.items.forEach(item => {
-      if (!['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros'].includes(item.name) && item.quality > 0 ) {
+      if (!['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros'].includes(item.name)
+        && item.quality > 0 ) { // decrase quailty
         item.quality = item.quality - 1
       }
 
-      if(['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert'].includes(item.name)) {
-        if (item.quality < 50) {
+      if(['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert'].includes(item.name) && item.quality < 50) { // increase quality
           item.quality = item.quality + 1
           if (item.name == 'Backstage passes to a TAFKAL80ETC concert' && item.sellIn < 11 && item.quality < 50) {
             item.quality = item.quality + 1
@@ -32,24 +32,25 @@ export class GildedRose {
             item.quality = item.quality + 1
           }
         }
-        }
       }
 
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+      if (item.name != 'Sulfuras, Hand of Ragnaros') { // decrease sellIn
         item.sellIn = item.sellIn - 1;
       }
 
-      if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.quality > 0 && item.name != 'Sulfuras, Hand of Ragnaros') {
-          item.quality = item.quality - 1
-        }
-        if(item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          item.quality = item.quality - item.quality
-        }
-        if(item.name =='Aged Brie' && item.quality < 50) {
-          item.quality = item.quality + 1
-        }
+      if (!['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros'].includes(item.name) // decrease quailty
+        && item.sellIn < 0  && item.quality > 0) {
+        item.quality = item.quality - 1
       }
+
+      if(item.sellIn < 0 && item.name == 'Backstage passes to a TAFKAL80ETC concert') { // make zero
+        item.quality = item.quality - item.quality
+      }
+
+      if(item.sellIn < 0 && item.name =='Aged Brie' && item.quality < 50) {
+        item.quality = item.quality + 1
+      }
+
     });
 
     return this.items;
