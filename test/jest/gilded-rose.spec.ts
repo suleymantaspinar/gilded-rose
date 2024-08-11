c53f121238a82310244bfd98f5e95540bcc5760f
Süleyman  Taşpınar <suleythegreat@Suleymans-MacBook-Pro.local>
2024-08-11 15:28:15 +0300

    test: cd '/Users/suleythegreat/side/gilded-rose'
    test: add tests for Gilded Rose suite
    
    test: add tests for Gilded Rose suite

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,48 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
+  it('should add a new item', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+
+    expect(items[0].name).toBe('foo');
   });
+
+  it('should add multiple items', () => {
+    const gildedRose = new GildedRose([
+      new Item('foo', 0, 0),
+      new Item('bar', 0, 0)
+    ]);
+
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].name).toBe('foo');
+    expect(items[1].name).toBe('bar');
+  });
+
+});
+
+
+describe('Gilded Rose', () => {
+  it('should add a new item', () => {
+    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].name).toBe('foo');
+  });
+
+  it('should add multiple items', () => {
+    const gildedRose = new GildedRose([
+      new Item('foo', 0, 0),
+      new Item('bar', 0, 0)
+    ]);
+
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].name).toBe('foo');
+    expect(items[1].name).toBe('bar');
+  });
+
 });
